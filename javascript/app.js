2f01dda39219574080de0f5869d374a96b5fa070
sillah2010 <macktastic2014@gmail.com>
2017-09-13 16:39:16 -0400

    Finished game logic, adding event Handlers

--- javascript/app.js
+++ javascript/app.js
@@ -5,133 +5,129 @@ $(() => {
     let playerTotal = 0;
     let dealerTotal = 0;
     let isRunning = false;
-  
+
     function makeCard(rank, suit) {
-      this.rank = rank;
-      this.suit = suit;
-  
-      let Card = {
-        rank: rank,
-        suit: suit
-      };
-      return Card;
-    }
-  
-    function deck() {
-      let newDeck = [];
-      const ranks = [
-        "Ace",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "Jack",
-        "Queen",
-        "King"
-      ];
-      const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
-  
-      for (let i = 0; i < suits.length; i++) {
-        for (let j = 0; j < ranks.length; j++) {
-          newDeck[i * ranks.length + j] = makeCard(ranks[j], suits[i]);
-        }
-      }
-      for (let k = 0; k < newDeck.length; k++) {
-        if (newDeck[k].rank === "Ace") {
-          newDeck[k].value = 1;
-        } else if (newDeck[k].rank === "2") {
-          newDeck[k].value = 2;
-        } else if (newDeck[k].rank === "3") {
-          newDeck[k].value = 3;
-        } else if (newDeck[k].rank === "4") {
-          newDeck[k].value = 4;
-        } else if (newDeck[k].rank === "5") {
-          newDeck[k].value = 5;
-        } else if (newDeck[k].rank === "6") {
-          newDeck[k].value = 6;
-        } else if (newDeck[k].rank === "7") {
-          newDeck[k].value = 7;
-        } else if (newDeck[k].rank === "8") {
-          newDeck[k].value = 8;
-        } else if (newDeck[k].rank === "9") {
-          newDeck[k].value = 9;
-        } else if (newDeck[k].rank === "10") {
-          newDeck[k].value = 10;
-        } else if (newDeck[k].rank === "Jack") {
-          newDeck[k].value = 10;
-        } else if (newDeck[k].rank === "Queen") {
-          newDeck[k].value = 10;
-        } else if (newDeck[k].rank === "King") {
-          newDeck[k].value = 10;
-        }
-      }
-      return newDeck;
-    }
-  
-  
-    function deal () {
-      isRunning = true;  
-      for (let i = 0; i < 2; i++) {
-        playerHand.push(mainDeck[Math.floor(Math.random() * (mainDeck.length - 0)) + 0]);
-        dealerHand.push(mainDeck[Math.floor(Math.random() * (mainDeck.length - 0)) + 0]);
-        // playerTotal += playerHand[i].value;
-        // dealerTotal += dealerHand[i].value;
-      }
-      //console.log("The player's hand is: " + JSON.stringify(playerHand));
-      //console.log("The players total: " + playerTotal);
-      //console.log("The dealer's hand is: " + JSON.stringify(dealerHand));
-      //console.log("The players total: " + dealerTotal);
+        this.rank = rank;
+        this.suit = suit;
+
+        let Card = {
+            rank: rank,
+            suit: suit
+        };
+        return Card;
     }
 
-    deal();
+    function deck() {
+        let newDeck = [];
+        const ranks = [
+            "Ace",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "Jack",
+            "Queen",
+            "King"
+        ];
+        const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < ranks.length; j++) {
+                newDeck[i * ranks.length + j] = makeCard(ranks[j], suits[i]);
+            }
+        }
+        for (let k = 0; k < newDeck.length; k++) {
+            if (newDeck[k].rank === "Ace") {
+                newDeck[k].value = 1;
+            } else if (newDeck[k].rank === "2") {
+                newDeck[k].value = 2;
+            } else if (newDeck[k].rank === "3") {
+                newDeck[k].value = 3;
+            } else if (newDeck[k].rank === "4") {
+                newDeck[k].value = 4;
+            } else if (newDeck[k].rank === "5") {
+                newDeck[k].value = 5;
+            } else if (newDeck[k].rank === "6") {
+                newDeck[k].value = 6;
+            } else if (newDeck[k].rank === "7") {
+                newDeck[k].value = 7;
+            } else if (newDeck[k].rank === "8") {
+                newDeck[k].value = 8;
+            } else if (newDeck[k].rank === "9") {
+                newDeck[k].value = 9;
+            } else if (newDeck[k].rank === "10") {
+                newDeck[k].value = 10;
+            } else if (newDeck[k].rank === "Jack") {
+                newDeck[k].value = 10;
+            } else if (newDeck[k].rank === "Queen") {
+                newDeck[k].value = 10;
+            } else if (newDeck[k].rank === "King") {
+                newDeck[k].value = 10;
+            }
+        }
+        return newDeck;
+    }
+    function deal() {
+        if (isRunning === false) {
+            isRunning = true;
+            for (let i = 0; i < 2; i++) {
+                playerHand.push(mainDeck[Math.floor(Math.random() * (mainDeck.length - 0)) + 0]);
+                dealerHand.push(mainDeck[Math.floor(Math.random() * (mainDeck.length - 0)) + 0]);
+            }
+        }
+    }
+
+
 
     function hit() {
-      if (isRunning === true && playerTotal <= 21) {
-        for (let i = 0; i < 1; i++) {  
-        playerHand.push(mainDeck[Math.floor(Math.random() * (mainDeck.length - 0)) + 0]);
-      }
-    }
-  
-    function stand() {
-      if (isRunning === true) {
-        isRunning = false;
-      }
-      while (dealerTotal <= 17) {
-        dealerHand.push(mainDeck[Math.floor(Math.random() * (mainDeck.length - 0)) + 0]);
-      }
-    }
-    // total value of hand = hand.reduce(function(sum, apple) {
-    // return sum + apple.value 
-    // })
-    function totalScores () {
-        playerTotal = playerHand.reduce(function(sum, value) {
-            return sum + value.value;
-        })
-        dealerTotal = dealerHand.reduce(function (pear, orange) {
-            return pear + orange.value;
-        })
-    
-    }
-  
-    function compare() {
-      if (playerTotal < dealerTotal) {
-        console.log("Dealer Wins");
-      } else if (playerTotal === 21 && dealerTotal < 21) {
-            console.log("You got Blackjack, you win");
-      } else if (playerTotal >= 21) {
-            console.log("BUSTED!");
-      } else if (dealerTotal >= 21) {
-            console.log("The dealer BUSTED. You win!");
-      }  else if (dealerTotal === 21 && playerTotal < 21) {
-            console.log("Dealer hit Blackjack. You lose");
-      }
+        if (isRunning === true) {
+            playerHand.push(mainDeck[Math.floor(Math.random() * (mainDeck.length - 0)) + 0]);
+        }
     }
 
+    function stand() {
+        if (isRunning === true) {
+            isRunning = false;
+        }
+        while (dealerTotal <= 17) {
+            dealerHand.push(mainDeck[Math.floor(Math.random() * (mainDeck.length - 0)) + 0]);
+        }
     }
-  });
-  
+
+    function totalScores() {
+        playerTotal = playerHand.reduce(function (sum, apple) {
+            return sum + apple.value
+        }, 0);
+        dealerTotal = dealerHand.reduce(function (pear, orange) {
+            return pear + orange.value
+        }, 0);
+    }
+
+    function compare() {
+        if (playerTotal < dealerTotal) {
+            console.log("Dealer Wins");
+        } else if (playerTotal === 21 && dealerTotal < 21) {
+            console.log("You got Blackjack, you win");
+        } else if (playerTotal >= 21) {
+            console.log("BUSTED!");
+        } else if (dealerTotal >= 21) {
+            console.log("The dealer BUSTED. You win!");
+        } else if (dealerTotal === 21 && playerTotal < 21) {
+            console.log("Dealer hit Blackjack. You lose");
+        }
+    }
+
+
+    function DeployGame() {
+        $('deal').on('click', function () {
+            deal();
+            console.log('Dealing cards now');
+        })
+    } 
+
+});
